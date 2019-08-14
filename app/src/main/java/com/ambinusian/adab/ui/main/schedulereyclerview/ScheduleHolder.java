4496dd5a48e201035eaf4064b1236e1e3cc7308b
brianwo <45139213+brianwo@users.noreply.github.com>
2019-08-15 03:30:55 +0700

    Fix and Beautify

--- app/src/main/java/com/ambinusian/adab/ui/main/schedulereyclerview/ScheduleHolder.java
+++ app/src/main/java/com/ambinusian/adab/ui/main/schedulereyclerview/ScheduleHolder.java
@@ -1,6 +1,6 @@
-package com.ambinusian.adab.ui.main.schedulereyclerview;
-
-import androidx.recyclerview.widget.RecyclerView;
-
-public class ScheduleHolder extends RecyclerView.ViewHolder{
-}
+//package com.ambinusian.adab.ui.main.schedulereyclerview;
+//
+//import androidx.recyclerview.widget.RecyclerView;
+//
+//public class ScheduleHolder extends RecyclerView.ViewHolder{
+//}
