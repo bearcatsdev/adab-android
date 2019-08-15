a80816ec441ebb7343135fcd6258beea1f56a2f0
Stanley <angstanley68@gmail.com>
2019-08-15 08:24:25 +0700

    Add LiveSession Activity

---
+++ app/src/main/java/com/ambinusian/adab/ui/livesession/LiveSessionActivity.java
@@ -0,0 +1,14 @@
+package com.ambinusian.adab.ui.livesession;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.ambinusian.adab.R;
+
+public class LiveSessionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_live_session);
+    }
+}
