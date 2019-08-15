e595350e87194ec2a9b3e720a006ef8728e5f56b
Chandra <developerchan1@gmail.com>
2019-08-15 12:15:50 +0700

    update scheduleholder

--- app/src/main/java/com/ambinusian/adab/ui/main/schedulereyclerview/ScheduleHolder.java
+++ app/src/main/java/com/ambinusian/adab/ui/main/schedulereyclerview/ScheduleHolder.java
@@ -1,6 +1,11 @@
 package com.ambinusian.adab.ui.main.schedulereyclerview;
 
+import android.view.View;
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class ScheduleHolder extends RecyclerView.ViewHolder{
+    public ScheduleHolder(@NonNull View itemView) {
+        super(itemView);
+    }
 }
