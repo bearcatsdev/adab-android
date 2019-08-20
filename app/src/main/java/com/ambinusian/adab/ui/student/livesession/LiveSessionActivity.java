416e3258aa456761a31f323b10f4f8624af47ce0
Stanley <angstanley68@gmail.com>
2019-08-20 19:40:06 +0700

    CollapsingAppBar
    hahahha

--- app/src/main/java/com/ambinusian/adab/ui/student/livesession/LiveSessionActivity.java
+++ app/src/main/java/com/ambinusian/adab/ui/student/livesession/LiveSessionActivity.java
@@ -1,5 +1,6 @@
 package com.ambinusian.adab.ui.student.livesession;
 
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -10,18 +11,24 @@ import com.ambinusian.adab.R;
 import com.ambinusian.adab.manager.APIManager;
 import com.ambinusian.adab.manager.NetworkHelper;
 import com.ambinusian.adab.preferences.UserPreferences;
+import com.github.nkzawa.emitter.Emitter;
+import com.github.nkzawa.socketio.client.IO;
+import com.github.nkzawa.socketio.client.Socket;
+import com.google.android.material.appbar.CollapsingToolbarLayout;
 import org.w3c.dom.Text;
 
+import java.net.URISyntaxException;
 import java.util.Map;
 
 public class LiveSessionActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
-    private TextView courseTitle;
     private TextView className;
     private TextView classSession;
     private TextView textContent;
     private TextView textLiveNow;
+    private Socket socket;
+    private CollapsingToolbarLayout ctl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,8 +42,8 @@ public class LiveSessionActivity extends AppCompatActivity {
             finish();
         }
 
+        ctl = findViewById(R.id.ctl);
         toolbar = findViewById(R.id.toolbar);
-        courseTitle = findViewById(R.id.tv_course_title);
         className = findViewById(R.id.tv_class_name);
         classSession = findViewById(R.id.tv_class_session);
         textContent = findViewById(R.id.tv_content);
@@ -46,7 +53,6 @@ public class LiveSessionActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayShowTitleEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
 
         APIManager apiManager = new APIManager(this);
         UserPreferences userPreferences = new UserPreferences(this);
@@ -56,14 +62,15 @@ public class LiveSessionActivity extends AppCompatActivity {
                 if (success) {
                     String courseTitleText = (String) classDetails.get("topic");
                     String classNameText = (String) classDetails.get("course_name");
-                    String sessionText = getString(R.string.class_session) + classDetails.get("session");
+                    String sessionText = getString(R.string.class_session) + " " + classDetails.get("session");
 
-                    courseTitle.setText(courseTitleText);
+                    ctl.setTitle(courseTitleText);
                     className.setText(classNameText);
                     classSession.setText(sessionText);
 
                     if ((int) classDetails.get("is_live") == 1) {
                         textLiveNow.setVisibility(View.VISIBLE);
+                        connectSocket();
                     }
                 }
             }
@@ -73,6 +80,34 @@ public class LiveSessionActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    private void connectSocket() {
+        try {
+            socket = IO.socket("https://adabapi.bancet.cf");
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        socket.on("kok lai liau", args -> {
+           runOnUiThread(() -> {
+               Log.d("aksjdkasjdkjasdkasd", args[0].toString());
+               textContent.append(args[0].toString() + "\n");
+           });
+        });
+        socket.connect();
+
+        while(!socket.connected()) {
+            Log.d("aksjdkasjdkjasdkasd", "connecting...");
+        }
+
+        if (socket.connected()) {
+            Log.d("aksjdkasjdkjasdkasd", "oke bang sudah konek");
+        } else {
+            Log.d("aksjdkasjdkjasdkasd", "bo huat");
+        }
+
+
+
     }
 
     @Override
