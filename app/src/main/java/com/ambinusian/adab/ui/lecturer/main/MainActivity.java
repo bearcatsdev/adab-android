0a4fb8d55ad13653f4f0fca4932a91f400b1a3a7
Stanley <angstanley68@gmail.com>
2019-08-24 00:26:10 +0700

    Add toolbar to lecturer MainActivity

--- app/src/main/java/com/ambinusian/adab/ui/lecturer/main/MainActivity.java
+++ app/src/main/java/com/ambinusian/adab/ui/lecturer/main/MainActivity.java
@@ -1,14 +1,44 @@
 package com.ambinusian.adab.ui.lecturer.main;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.FragmentManager;
+
 import android.os.Bundle;
 import com.ambinusian.adab.R;
+import com.ambinusian.adab.ui.userprofile.UserProfileDialogFragment;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    Toolbar toolbar;
+    CircleImageView profilePicture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_lecturer);
+
+        toolbar = findViewById(R.id.tool_bar);
+        profilePicture = findViewById(R.id.circle_image_profile_picture);
+
+        // set toolbar
+        setSupportActionBar(toolbar);
+
+        // remove title
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        // profile picture onclick
+        profilePicture.setOnClickListener(v -> {
+            showUserProfileDialog();
+        });
+    }
+
+    private void showUserProfileDialog() {
+        FragmentManager fm = getSupportFragmentManager();
+        UserProfileDialogFragment userProfileDialogFragment = new UserProfileDialogFragment();
+        userProfileDialogFragment.show(fm, "fragment_user_profile_dialog");
     }
 }
