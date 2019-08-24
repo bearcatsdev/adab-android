3714137933c08aacba1aa190fe0fec8f2644f535
Chandra <developerchan1@gmail.com>
2019-08-25 00:04:02 +0700

    Update layout for discussion

--- app/src/main/java/com/ambinusian/adab/ui/student/main/discussionrecyclerview/DiscussionHolder.java
+++ app/src/main/java/com/ambinusian/adab/ui/student/main/discussionrecyclerview/DiscussionHolder.java
@@ -4,12 +4,14 @@ import android.view.View;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.ambinusian.adab.R;
 
 public class DiscussionHolder extends RecyclerView.ViewHolder {
     public TextView discussion_time, discussion_content, discussion_name, discussion_class;
+    public CardView card_view_layout;
 
     public DiscussionHolder(@NonNull View itemView) {
         super(itemView);
@@ -17,5 +19,8 @@ public class DiscussionHolder extends RecyclerView.ViewHolder {
         discussion_content = itemView.findViewById(R.id.tv_discussion_content);
         discussion_name = itemView.findViewById(R.id.tv_discussion_name);
         discussion_class = itemView.findViewById(R.id.tv_discussion_class);
+        card_view_layout = itemView.findViewById(R.id.card_view_layout);
+
+        card_view_layout.setBackgroundResource(R.drawable.chat_item_layout);
     }
 }
