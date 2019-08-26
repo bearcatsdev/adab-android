c71bfa1374a99a22ce98d99aacf1138cfb226df9
Chandra <developerchan1@gmail.com>
2019-08-26 18:11:38 +0700

    Update login layout

---
+++ app/src/main/java/com/ambinusian/adab/manager/FragmentChangeListener.java
@@ -0,0 +1,7 @@
+package com.ambinusian.adab.manager;
+
+import androidx.fragment.app.Fragment;
+
+public interface FragmentChangeListener {
+    public void replaceFragment(Fragment fragment);
+}
--- app/src/main/java/com/ambinusian/adab/ui/login/LoginActivity.java
+++ app/src/main/java/com/ambinusian/adab/ui/login/LoginActivity.java
@@ -4,80 +4,39 @@ import android.content.Context;
 import android.content.Intent;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
 import android.os.Bundle;
 import com.ambinusian.adab.R;
 import com.ambinusian.adab.manager.APIManager;
+import com.ambinusian.adab.manager.FragmentChangeListener;
 import com.ambinusian.adab.manager.NetworkHelper;
 import com.ambinusian.adab.preferences.UserPreferences;
-import com.ambinusian.adab.ui.splash.SplashActivity;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
 
 import java.util.Map;
 
-public class LoginActivity extends AppCompatActivity {
-
-    MaterialButton btnLogin;
-    TextInputEditText inputNim;
-    TextInputEditText inputPassword;
+public class LoginActivity extends AppCompatActivity implements FragmentChangeListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        btnLogin = findViewById(R.id.btn_login);
-        inputNim = findViewById(R.id.input_nim);
-        inputPassword = findViewById(R.id.input_password);
-
-        btnLogin.setOnClickListener(view -> {
-            doLogin();
-        });
+        getSupportFragmentManager().beginTransaction().replace(R.id.login_layout,new WelcomeFragment()).commit();
     }
 
-    private void doLogin() {
-        boolean valid = true;
 
-        if (inputPassword.length() < 1) {
-            valid = false;
-            inputPassword.setError(getString(R.string.error_password_empty));
-            inputPassword.requestFocus();
-        }
-
-        if (inputNim.length() != 10 && inputNim.length() != 5) {
-            valid = false;
-            inputNim.setError(getString(R.string.error_nim_invalid));
-            inputNim.requestFocus();
-        }
-
-        if (valid) {
-            Context context = this;
-            APIManager apiManager = new APIManager(context);
-
-            apiManager.authenticateUser(inputNim.getText().toString(), inputPassword.getText().toString(), new NetworkHelper.authenticateUser() {
-                @Override
-                public void onResponse(Boolean success, Map<String, Object> userProfile) {
-                    if (success) {
-                        UserPreferences userPreferences = new UserPreferences(context);
-                        userPreferences.setUserLoggedIn(true);
-                        userPreferences.setUserUsername((String) userProfile.get("username"));
-                        userPreferences.setUserToken((String) userProfile.get("token_id"));
-                        startActivity(new Intent(LoginActivity.this, SplashActivity.class));
-                        finish();
-                    }
-                }
-
-                @Override
-                public void onError(int errorCode, String errorReason) {
-                    if (errorCode == 403) {
-                        inputNim.setError(getString(R.string.username_or_password_invalid));
-                        inputPassword.setError(getString(R.string.username_or_password_invalid));
-                        inputNim.requestFocus();
-                    } else {
-                        Toast.makeText(context, getString(R.string.unknown_error), Toast.LENGTH_LONG).show();
-                    }
-                }
-            });
-        }
+    @Override
+    public void replaceFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.setCustomAnimations(R.anim.slide_in_left, R.anim.slide_out_left,
+                R.anim.slide_out_right, R.anim.slide_in_right);
+        fragmentTransaction.replace(R.id.login_layout,fragment,fragment.toString());
+        fragmentTransaction.addToBackStack(fragment.toString());
+        fragmentTransaction.commit();
     }
 }
